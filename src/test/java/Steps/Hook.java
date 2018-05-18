7a3f2040a6574adfe7fe29bce95781609b242154
abfedena <abfedena@users.noreply.github.com>
2018-05-19 01:12:18 +0530

    Thius is commits

--- src/test/java/Steps/Hook.java
+++ src/test/java/Steps/Hook.java
@@ -24,6 +24,9 @@ public class Hook extends BaseUtil{
 
         //Passing a dummy WebDriver instance step info
         base.StepInfo = "FirefoxDriver";
+        
+        System.setProperty("webdriver.chrome.driver", "C:\chromedriver_win32\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
     }
 
 
